78ee077aa1cc8021bf88148ed7daa32193da7a55
Jesse Lawson <jesse@lawsonry.com>
2016-12-23 11:58:23 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// This is meant to be a copy+pasteable project template for SDL2
+// I'm using CLion, so YMMV.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
